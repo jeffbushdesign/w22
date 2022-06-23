2bd8ac34b677917b1f0cae74cee85ab478286c0f
Jeff Bush <jeffbushdesign@gmail.com>
2022-06-23 10:10:54 -0400

    Add missing exercises

---
+++ learnTheHardWayExercises/ex5.rb
@@ -0,0 +1,10 @@
+my_name = "Jeff Bush"
+my_age = 36
+my_height = 76 #inches
+my_weight = 210
+my_eyes = 'Hazel'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+puts "Let's talk about #{my_name}."
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
